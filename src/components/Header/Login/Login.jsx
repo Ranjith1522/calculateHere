ca88d7ffa8bce24edb70dec74de05860cc5b03f5
ranjith <ranjithkankati2@gmail.com>
2026-02-18 13:07:50 +0530

    login screen popup completed

--- src/components/Header/Login/Login.jsx
+++ src/components/Header/Login/Login.jsx
@@ -1,18 +1,28 @@
 import './Login.css'
 
-function Login() {
+function Login({onClose}) {
+
+    function logIn() {
+        console.log("Login button clicked");
+    }
 
     return <>
-        <div className='modal-overlay'>
-            <div className="card">
+        <div className='modal-overlay' onClick={onClose}>
+            <div className="card" onClick={(e) => {e.stopPropagation()}}>
+                <div>
+                    <h3>User Login Dashboard</h3>
+                </div>
                 <div className="user-name">
                     <label>UserName : </label>
-                    <input type='text' />
+                    <input className='userinput' type='text' />
                 </div>
                 <br></br>
                 <div className="user-password">
                     <label>Password : </label>
-                    <input type='password' />
+                    <input className='userpassword' type='password' />
+                </div>
+                <div className='userlogin'>
+                    <button className='btn' onClick={logIn}>Login</button>
                 </div>
                 
             </div>
