a57f5aea66cc8e89e32271469f77fc92ab20d7f9
ranjith <ranjithkankati2@gmail.com>
2026-02-19 17:55:17 +0530

    login created

--- src/components/Header/Login/Login.jsx
+++ src/components/Header/Login/Login.jsx
@@ -1,8 +1,16 @@
+import { useState } from 'react';
 import './Login.css'
 
 function Login({onClose}) {
 
+    const [username, setUsername] = useState("");
+    const [password,setPassword] = useState("");
+
     function logIn() {
+        if(!username || !password) {
+            alert("Please enter user name and password");
+            return;
+        }
         console.log("Login button clicked");
     }
 
@@ -14,12 +22,12 @@ function Login({onClose}) {
                 </div>
                 <div className="user-name">
                     <label>UserName : </label>
-                    <input className='userinput' type='text' />
+                    <input className='userinput' type='text' value={username} onChange={(e) => setUsername(e.target.value)}/>
                 </div>
                 <br></br>
                 <div className="user-password">
                     <label>Password : </label>
-                    <input className='userpassword' type='password' />
+                    <input className='userpassword' type='password' value={password} onChange={(e) => setPassword(e.target.value)}/>
                 </div>
                 <div className='userlogin'>
                     <button className='btn' onClick={logIn}>Login</button>
