4d8173132cae43c5e5fb30150160ef9b864cf696
ranjith <ranjithkankati2@gmail.com>
2026-02-21 23:53:25 +0530

    dashboard component added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+import Dashboard from './components/Dashboard/Dashboard'
 
 function App() {
 
   return (
     <>
     <Header/>
+    <Dashboard/>
     <Footer/>
     </>
   )
---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import './Dashboard.css'
+
+function Dashboard() {
+    return <>
+
+    <div className="dashboard-main">
+        {/* <p>content</p> */}
+    </div>
+    </>
+}
+
+export default Dashboard;
